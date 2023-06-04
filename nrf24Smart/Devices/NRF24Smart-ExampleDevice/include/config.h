497afd96ab905ff1bda0c557d4ef6696838d5d7f
Marcus Voß <46140304+mvoss96@users.noreply.github.com>
2023-06-04 15:34:44 +0200

    added EXAMPLE Device

---
+++ nrf24Smart/Devices/NRF24Smart-ExampleDevice/include/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define DEVICE_TYPE "TEST_DEVICE"
+#define DEVICE_BATTERY_POWERED 0                // 1 for battery
+#define DEVICE_UUID { 0xB8, 0x6F, 0xA2, 0xD3 } // Unique 4 byte UUID
+#define FIRMWARE_VERSION 0x01
+#define RADIO_CHANNEL 101
---
+++ nrf24Smart/Devices/NRF24Smart-ExampleDevice/src/main.cpp
@@ -0,0 +1,43 @@
+#include <Arduino.h>
+#include "RFcomm.h"
+
+void (*resetFunc)(void) = 0;
+
+void setup()
+{
+  Serial.begin(115200);
+  //resetEEPROM();
+  //delay(100000000);
+  connectToServer();
+  printEEPROM(5);
+  delay(1000);
+}
+
+void loop()
+{
+  if (!serverConnected)
+  {
+    Serial.println("Connecting to Server:");
+    connectToServer();
+  }
+  // Listen for new Messages
+  uint8_t packetSize = _radio.hasData();
+  uint8_t buf[32] = {0};
+  if (packetSize > 0)
+  {
+    _radio.readData(&buf);
+    ServerPacket pck(buf, packetSize);
+    if (pck.isValid())
+    {
+      switch ((MSG_TYPES)pck.getTYPE())
+      {
+      case MSG_TYPES::RESET:
+        Serial.println("RESET message received: resetting Device...");
+        resetEEPROM();
+        break;
+      default:
+        Serial.println("Unsupported message received!");
+      }
+    }
+  }
+}
