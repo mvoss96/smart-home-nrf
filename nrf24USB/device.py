2098bb63eeed9f86d5e6f350edcb8130ad94e0af
Marcus Voß <m.voss@laposte.net>
2023-07-25 20:01:00 +0200

    added more docstrings

--- nrf24USB/device.py
+++ nrf24USB/device.py
@@ -10,7 +10,15 @@ from .packet_reader import PacketReader, SpecialBytes, MSG_TYPES
 
 
 class NRF24Device:
+    """
+    Class for NRF24Device that provides functionalities to start read loop, initialize the device,
+    send packets and messages, get messages, handle packets, read loop, stop read loop and destructor.
+    """
     def __init__(self, port: str, channel: int, address: int, baudrate=115200):
+        """
+        Initializes the NRF24Device with provided port, channel, address, and baudrate.
+        Raises ValueError if the provided channel and address are out of allowed range.
+        """
         if not 0 <= channel <= 125:
             raise ValueError("Channel must be between 0 and 125.")
         if not 0 <= address <= 255:
@@ -27,12 +35,19 @@ class NRF24Device:
         self.reader = PacketReader(self.serial_port)
 
     def start_read_loop(self):
+        """
+        Starts the read loop in a new thread. If the read thread is already running, stops it before starting a new one.
+        """
         if self.read_thread is not None and self.read_thread.is_alive():
             self.stop_read_loop()
         self.read_thread = threading.Thread(target=self.read_loop, name="read_loop")
         self.read_thread.start()  # Start a new thread that runs the read_loop function
 
     def initialize_device(self):
+        """
+        Initializes the NRF24USB device. Checks and sets the INIT message from the device. Raises ConnectionError if the device 
+        does not send correct INIT message or does not react correctly to Host INIT message.
+        """
         logging.info(f"Initialize NRF24USB Device with channel: {self.channel} address: {self.address} ...")
 
         # Try to Read the INIT message of the device
@@ -57,6 +72,9 @@ class NRF24Device:
         self.connected = True
 
     def send_packet(self, data: bytes, msg_type: MSG_TYPES):
+        """
+        Sends packet to the NRF24USB device. Takes bytes of data and message type as arguments.
+        """
         logging.debug(f"sending packet {msg_type} {data}")
         self.serial_port.write(bytes([SpecialBytes.START_BYTE]))  # Write the start byte
         self.serial_port.write(bytes([msg_type.value]))  # Write the packet type
@@ -70,6 +88,10 @@ class NRF24Device:
         self.serial_port.write(bytes([SpecialBytes.END_BYTE]))  # Write the end byte
 
     def send_msg(self, destination: int, data: list[int], require_ack=True) -> Optional[list[int]]:
+        """
+        Sends message to the destination. Takes destination, list of integers as data, and acknowledgement requirement as arguments.
+        Returns received data on successful acknowledgement or None otherwise.
+        """
         raw_msg_hex = [hex(i) for i in data]
         with self.lock:
             print(f"send to id:{destination} {raw_msg_hex}")
@@ -87,6 +109,9 @@ class NRF24Device:
                 return None
 
     def get_message(self):
+        """
+        Retrieves and returns the message from the queue. Returns None if the queue is empty.
+        """
         while True:
             try:
                 (type, data) = self.msg_queue.get(block=True, timeout=0)
@@ -96,6 +121,10 @@ class NRF24Device:
                 return None
 
     def handle_packet(self, packet_type: MSG_TYPES, packet_data: list[int]):
+        """
+        Handles packet based on the type. Logs error for ERROR type, attempts to reconnect for INIT type, 
+        puts the message into the queue for MSG type, and logs warning for other types.
+        """
         if packet_type == MSG_TYPES.ERROR:
             logging.error(
                 f"NRF24USB Device reported ERROR: {bytes(packet_data).decode(errors='ignore') if packet_data is not None else ''}"
@@ -111,6 +140,10 @@ class NRF24Device:
             logging.warning(f"MSG_TYPE {packet_type} {packet_data} was ignored!")
 
     def read_loop(self):
+        """
+        Starts the read loop for processing bytes until stop event is set. Handles the packets read by the reader.
+        Logs an exception if any error occurs during the read loop.
+        """
         logging.info("NRF24USB Read Loop Started!")
         try:
             while not self.stop_event.is_set():  # Inner loop for processing bytes
@@ -128,6 +161,9 @@ class NRF24Device:
         logging.info("NRF24USB Read Loop Stopped!")
 
     def stop_read_loop(self):
+        """
+        Stops the read loop if the read thread is not None. Waits for the thread to finish if called from another thread.
+        """
         if self.read_thread != None:
             logging.info("Stopping Read Loop ...")
             self.stop_event.set()  # Set the stop event
@@ -135,4 +171,8 @@ class NRF24Device:
                 self.read_thread.join()  # Wait for the thread to finish if called from another thread
 
     def __del__(self):
+        """
+        Stops the read loop and closes the serial port when the instance is destructed.
+        """
         self.stop_read_loop()
+        self.serial_port.close()
